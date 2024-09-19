cab313e4cb4d3ff23b590cd804b561de721b1755
MardanFuMardan <mahmudhegazy007@gmail.com>
2024-09-19 13:50:03 +0300

    add github-user-search

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,13 +1,9 @@
-import React from 'react';
-import Search from './components/Search';
+import React from 'react'
 
 function App() {
   return (
-    <div>
-      <h1  className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 sm:max-w-xs md:p-8 md:max-w-sm hover:shadow-xl">GitHub User Search Application</h1>
-      <Search />
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
